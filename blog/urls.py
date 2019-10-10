1b5749ecebd7747c9af0670d4262904e0917255a
mayank <sainimayank70@gmail.com>
2019-10-10 22:26:15 +0530

    drafts , remove and delete features are added

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ path('',views.post_list,name='post_list' ),
 path('post/<int:pk>/', views.post_detail, name='post_detail'),
 path('post/new/', views.post_new, name='post_new'),
 path('post/<int:pk>/edit/',views.post_edit, name= 'post_edit'),
+path('drafts/',views.post_draft_list ,name='post_draft_list'),
+path('post/<pk>/publish', views.post_publish, name='post_publish'),
+path('post/<pk>/delete', views.post_delete, name='post_delete'),
 ]
