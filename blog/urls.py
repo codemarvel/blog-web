225a38a1f97ec57ad8e8b8e8bf685438d8282aeb
mayank <sainimayank70@gmail.com>
2019-10-11 22:07:33 +0530

    comment model is added

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,8 @@ path('post/<int:pk>/edit/',views.post_edit, name= 'post_edit'),
 path('drafts/',views.post_draft_list ,name='post_draft_list'),
 path('post/<pk>/publish', views.post_publish, name='post_publish'),
 path('post/<pk>/delete', views.post_delete, name='post_delete'),
+path('post/<int:pk>/comment',views.add_comment_to_post,name='add_comment_to_post'),
+path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 
 ]
